1e03fe81993ed5933ec12324a8a6e69cb743cda1
victor47n <victor47n@gmail.com>
2021-02-27 00:11:31 -0300

    Aula 3 e algumas correções de layout

--- moveit-next/src/components/CompletedChallenges.tsx
+++ moveit-next/src/components/CompletedChallenges.tsx
@@ -1,10 +1,14 @@
-import styles from '../styles/components/CompletedChallanges.module.css';
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContext';
+import styles from '../styles/components/CompletedChallenges.module.css';
 
 export function CompletedChanllenges() {
+    const { challengesCompleted } = useContext(ChallengesContext)
+
     return (
-        <div className={styles.completedChallangesContainer}>
+        <div className={styles.completedChallengesContainer}>
             <span>Desafios completos</span>
-            <span>5</span>
+            <span>{challengesCompleted}</span>
         </div>
     )
 }
