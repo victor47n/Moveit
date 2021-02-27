c82b9d8fd414a66de83ca374340849140113bebe
victor47n <victor47n@gmail.com>
2021-02-27 00:52:14 -0300

    Quarta aula

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext)
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/victor47n.png" alt="Victor Hugo" />
@@ -8,8 +12,8 @@ export function Profile() {
                 <strong>Victor Hugo</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1
-                    </p>
+                    Level {level}
+                </p>
             </div>
         </div>
     );
